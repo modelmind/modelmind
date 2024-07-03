4b4c2ec7e563edf38273d0a3aa515e9d916d8976
Bryan Djafer <yukky.bryan@gmail.com>
2024-07-03 23:31:15 +0200

    ci/cd

--- modelmind/api/public/__init__.py
+++ modelmind/api/public/__init__.py
@@ -1,9 +1,9 @@
 from fastapi import APIRouter
 
+from .endpoints.health import router as health_router
 from .endpoints.profile import router as profile_router
 from .endpoints.questionnaire import router as questionnaire_router
 from .endpoints.results import router as results_router
-from .endpoints.health import router as health_router
 
 public_v1_router = APIRouter(prefix="/v1/public", tags=["Public"])
 public_v1_router.include_router(router=profile_router)
