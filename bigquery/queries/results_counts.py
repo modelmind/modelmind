1ca76da62fe573db555d456261fac196c2cc09e5
Bryan Djafer <yukky.bryan@gmail.com>
2024-07-05 23:42:28 +0200

    query: edit counts

--- bigquery/queries/results_counts.py
+++ bigquery/queries/results_counts.py
@@ -1,7 +1,7 @@
 QUERY="""
 SELECT
-  r.result_label AS label,
-  COUNT(*) AS counts,
+  COUNT(*) AS total,
+  COUNT(p.biographics_personality_mbti_type) AS with_label,
   SUM(CASE WHEN p.biographics_gender = 'male' THEN 1 ELSE 0 END) AS male,
   SUM(CASE WHEN p.biographics_gender = 'female' THEN 1 ELSE 0 END) AS female,
   SUM(CASE WHEN p.biographics_gender NOT IN ('male', 'female') THEN 1 ELSE 0 END) AS other
@@ -10,9 +10,5 @@ FROM
 JOIN
   `modelmind.firestore_sync.profiles_schema_views_latest` p
 ON
-  r.result_id = p.results_member
-GROUP BY
-  r.result_label
-ORDER BY
-  r.result_label;
+  r.result_id = p.results_member;
 """
