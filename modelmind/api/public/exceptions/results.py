66bfd4b1dd9f19c0bb2d75dcf3c6e3ca1e4ca3bd
Bryan Djafer <yukky.bryan@gmail.com>
2024-07-07 18:37:58 +0200

    fix: forbidden exception instead of not found

--- modelmind/api/public/exceptions/results.py
+++ modelmind/api/public/exceptions/results.py
@@ -1,8 +1,8 @@
-from modelmind.api.exceptions import NotFoundException
+from modelmind.api.exceptions import ForbiddenException
 from modelmind.logger import log
 
 
-class ResultAccessForbiddenException(NotFoundException):
+class ResultAccessForbiddenException(ForbiddenException):
     def __init__(self, profile_id: str | None, result_id: str):
         log.warning("Profile with ID %s does not have access to result with ID %s", profile_id, result_id)
         detail = f"Profile with ID {profile_id} does not have access to result with ID {result_id}"
